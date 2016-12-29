be6ff0fb5d31e40ba43f19f4e0fa7e006ec69715
Ogi Moore <ognyan.moore@gmail.com>
2016-12-29 10:05:01 -0800

    ensured diff-so-fancy is used

--- post-process.sh
+++ post-process.sh
@@ -7,9 +7,12 @@ source ~/.bash_profile
 # Configure R and JAVA
 R CMD javareconf JAVA_CPPFLAGS=-I/System/Library/Frameworks/JavaVM.framework/Headers
 
-#Link subl to sublime-text
+# Link subl to sublime-text
 mkdir -p ~/bin && ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
-#Installing nano syntax coloring
+# Installing nano syntax coloring
 git clone git@github.com:scopatz/nanorc.git ~/.nano
 cat ~/.nano/nanorc >> ~/.nanorc
+
+# Ensuring diff-so-fancy is used exclusively
+diff config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
